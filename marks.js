0b4a353c57ba46cc2bd836b0c509f165caf47cbc
MUNEER HUSSAIN <90671155+muneermir@users.noreply.github.com>
2025-06-20 23:29:12 -0700

    Create marks.js

---
+++ marks.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <script>
+        let marks =prompt("enter your marks");
+        if (marks>33)
+     {
+        console.log("pass")
+     }
+     else if ( marks >=33 && marks <=60) {
+
+     
+        console.log("Third Division")
+     }
+
+     else if (marks>60 && marks<80){
+     console.log("second division")}
+
+     else if (marks>80 && marks<=90)
+     {
+      console.log("first division")
+     }
+      else  (marks>90 && marks<100)
+     {
+      console.log("Excellent")
+     }
+     </script>
+</body>
+</html>
